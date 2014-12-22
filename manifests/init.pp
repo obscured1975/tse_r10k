d0392402730afd075ebfe93daa296a6a913cf65a
John Hong <obscured1975@gmail.com>
2014-12-22 14:58:44 -0500

    Added main production environment source

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,11 @@ class tse_r10k {
   class { 'r10k':
     provider => 'pe_gem',
     sources => {
+      'enterprise' => {
+        'remote'  => 'https://github.com/puppetlabs/seteam-puppet-environments.git',
+        'basedir' => "/etc/puppetlabs/puppet/environments",
+        'prefix'  => false,
+      },
       'businessunit1' => {
         'remote'  => 'https://github.com/obscured1975/businessunit1.git',
         'basedir' => "/etc/puppetlabs/puppet/environments",
